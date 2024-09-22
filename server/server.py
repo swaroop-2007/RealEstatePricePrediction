5afa0aa17d21e77ba8879f42baec6ec48c79e219
swaroop-2007 <73997421+swaroop-2007@users.noreply.github.com>
2024-09-22 12:36:05 -0400

    Update: Built get and post endpoints to print the location and output the estimated price

--- server/server.py
+++ server/server.py
@@ -4,14 +4,27 @@ app = Flask(__name__)
 
 
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
+    print(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
+
+@app.route('/predict_house_price', methods=['POST'])
+def predict_house_price():
+    totalsqft = float(request.form['totalsqft'])
+    location = request.form['location']
+    bedrooms = int(request.form['bedrooms'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.getPrice(location,totalsqft, bedrooms,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response 
 
 
 if __name__ == '__main__':
--- server/util.py
+++ server/util.py
@@ -1,27 +1,55 @@
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns = None
 __model = None
 
-def get_location_names():
-    return __locations
-
 def getArtficats():
     global __data_columns
     global __locations
+    global __model
 
-    with open('./artifacts/columns.json', 'r') as f:
-        __data_columns = json.load(f)['data_cols']
-        __locations = __data_columns[3:]
+    # Check if the artifacts have already been loaded to avoid redundant loading
+    if __data_columns is None or __locations is None or __model is None:
+        print("Loading artifacts...")
 
-    with open('./artifacts/realestatepricesmodel.pickle', 'rb') as f:
-        __model = pickle.load(f)
+        with open('./artifacts/columns.json', 'r') as f:
+            __data_columns = json.load(f)['data_cols']
+            __locations = __data_columns[3:]
+
+        with open('./artifacts/realestatepricesmodel.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    else:
+        print("Artifacts already loaded.")
+
+
+def get_location_names():
+    # Ensure that the artifacts are loaded before returning locations
+    getArtficats()
+    return __locations
+
+def getPrice(location, sqft, bedrooms, bath):
+    # Ensure that the artifacts are loaded before calculating price
+    getArtficats()
+
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
     
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bedrooms
+    if loc_index >= 0:
+        x[loc_index] = 1
 
+    return round(__model.predict([x])[0], 2)
 
 
 if __name__ == '__main__':
     getArtficats()
     print(get_location_names())
+    print(getPrice('1st Phase JP Nagar', 1000, 3, 3))
